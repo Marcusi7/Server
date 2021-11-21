f428b6940b203b4e14532304d961f94e2cc27f3b
Marcusi7 <mealtamirano@puce.edu.ec>
2021-11-21 16:12:34 -0500

    Co-authored-by: sochi2707 <sochi2707@users.noreply.github.com> Co-authored-by: RIcki-cpu <RIcki-cpu@users.noreply.github.com>

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -5,29 +5,4 @@
  * to customize this controller
  */
 
-module.exports = {
-    /*async create(ctx) {
-        const { articulos, address, addressShipping } = ctx.request.body;
-        let totalPayment = 0;
-        articulos.forEach(articulo => {
-            totalPayment = totalPayment + articulo.price;
-        });
-
-        const createOrder =  [];
-        for await (const articulo of articulos) {
-            const data = {
-                product: articulo.id,
-                user: address._id,
-                //totalPayment,
-                addressShipping,
-            };
-            const validData = await strapi.entityValidator.validateEntity(
-                strapi.models.order,
-                data
-            );
-            const entry = await strapi.query("order").create(validData);
-            createOrder.push(entry);
-        }
-        return createOrder;
-    },*/
-};
+module.exports = {};
